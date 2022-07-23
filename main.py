45e95405e564d6fb870778b1de67e22dad119b46
RAHU1508 <rahulxyz96@gmail.com>
2022-07-23 17:32:48 +0530

    mongodb

--- main.py
+++ main.py
@@ -2,8 +2,71 @@ import pymongo
 client = pymongo.MongoClient("mongodb+srv://RAHU1508:mongodb123@rahu1508.g93ki.mongodb.net/?retryWrites=true&w=majority")
 db = client.test
 print(db)
-
-d = {'name':'rahul', 'surname':'kumar'}
 db1 = client['mongotest']
-coll = db1['test']
-coll.insert_one(d)
+coll = db1['test3']
+data1 = [
+{
+            "item": "canvas",
+            "qty": 100,
+            "size": {"h": 28, "w": 35.5, "uom": "cm"},
+            "status": "A",
+        },
+        {
+            "item": "journal",
+            "qty": 25,
+            "size": {"h": 14, "w": 21, "uom": "cm"},
+            "status": "A",
+        },
+        {
+            "item": "mat",
+            "qty": 85,
+            "size": {"h": 27.9, "w": 35.5, "uom": "cm"},
+            "status": "A",
+        },
+        {
+            "item": "mousepad",
+            "qty": 25,
+            "size": {"h": 19, "w": 22.85, "uom": "cm"},
+            "status": "P",
+        },
+        {
+            "item": "notebook",
+            "qty": 50,
+            "size": {"h": 8.5, "w": 11, "uom": "in"},
+            "status": "P",
+        },
+        {
+            "item": "paper",
+            "qty": 100,
+            "size": {"h": 8.5, "w": 11, "uom": "in"},
+            "status": "D",
+        },
+        {
+            "item": "planner",
+            "qty": 75,
+            "size": {"h": 22.85, "w": 30, "uom": "cm"},
+            "status": "D",
+        },
+        {
+            "item": "postcard",
+            "qty": 45,
+            "size": {"h": 10, "w": 15.25, "uom": "cm"},
+            "status": "A",
+        },
+        {
+            "item": "sketchbook",
+            "qty": 80,
+            "size": {"h": 14, "w": 21, "uom": "cm"},
+            "status": "A",
+        },
+        {
+            "item": "sketch pad",
+            "qty": 95,
+            "size": {"h": 22.85, "w": 30.5, "uom": "cm"},
+            "status": "A",
+        }
+]
+coll.insert_many(data1)
+record = coll.find({'item': 'sketch pad','qty':{'$gte':1008}})
+for i in record:
+    print(i)
